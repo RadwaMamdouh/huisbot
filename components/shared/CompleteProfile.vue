f2e615a52212b92854144716e93118630ccf737f
Radwa Mamdouh <rmamdouh7979@gmail.com>
2021-11-17 21:07:12 +0200

    enhance complete profile modal

--- components/shared/CompleteProfile.vue
+++ components/shared/CompleteProfile.vue
@@ -7,26 +7,30 @@
     overlay-opacity="0.70"
   >
     <v-card class="complete_profile_dialog--content">
-      <div class="head">
-        <div class="title">Complete Profile</div>
-        <v-btn text @click="dialog = false" class="close_btn">
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </div>
-      <div class="invite_more">
-        <div class="left">
-          <div class="invite_more--tit">Complete Your Profile</div>
-          <div class="invite_more--desc">
-            Your profile need to be completed to get properties information and
+      <img src="@/assets/img/complete-profile-bg.png" alt="" class="img_bg" />
+      <div class="wrapper">
+        <div class="head">
+          <div class="title">Complete Profile</div>
+          <v-btn text @click="dialog = false" class="close_btn">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </div>
+        <div class="invite_more">
+          <div class="left">
+            <div class="invite_more--tit">Complete Your Profile</div>
+            <div class="invite_more--desc">
+              Your profile need to be completed to get properties information
+              and
+            </div>
+          </div>
+          <div class="right">
+            <img src="@/assets/img/complete-profile.png" alt="" />
           </div>
         </div>
-        <div class="right">
-          <img src="@/assets/img/complete-profile.png" alt="" />
+        <div class="dialog_actions">
+          <v-btn class="not_now" text @click="dialog = false">Not Now</v-btn>
+          <v-btn class="complete" text>Let’s Complete</v-btn>
         </div>
-      </div>
-      <div class="dialog_actions">
-        <v-btn class="not_now" text @click="dialog = false">Not Now</v-btn>
-        <v-btn class="complete" text>Let’s Complete</v-btn>
       </div>
     </v-card>
   </v-dialog>
@@ -38,7 +42,7 @@ export default {
 
   data() {
     return {
-      dialog: false,
+      dialog: true,
     };
   },
 };
@@ -48,10 +52,6 @@ export default {
 .complete_profile_dialog {
   max-width: 50%;
   box-shadow: none !important;
-  background-image: url("@/assets/img/complete-profile-bg.png");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
 
   @media (max-width: 1400.98px) {
     max-width: 55%;
@@ -68,10 +68,36 @@ export default {
   &--content {
     background-color: transparent !important;
     box-shadow: none !important;
-    padding: 106px 60px;
+    padding: 20px;
 
     @media (max-width: 600.98px) {
-      padding: 50px 20px;
+      padding: 0;
+      margin: 0;
+    }
+
+    .img_bg {
+      width: 100%;
+      height: 100%;
+      // max-height: 400px;
+
+      @media (max-width: 600.98px) {
+        margin: 0;
+        height: 255px;
+      }
+    }
+
+    .wrapper {
+      position: absolute;
+      z-index: 1;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      padding: 100px 100px 0;
+
+      @media (max-width: 600.98px) {
+        padding: 30px 35px;
+      }
     }
 
     .head {
@@ -109,6 +135,7 @@ export default {
 
       @media (max-width: 600.98px) {
         column-gap: 10px;
+        margin-bottom: 15px;
       }
 
       &--tit {
@@ -135,6 +162,7 @@ export default {
       .right {
         img {
           @media (max-width: 600.98px) {
+            width: 100%;
             max-width: 120px;
           }
         }
